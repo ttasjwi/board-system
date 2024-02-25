1609206b060d098639f3c40218688b9469408800
ttasjwi <ttasjwi920@gmail.com>
2024-02-25 23:56:44 +0900

    Initial commit

---
+++ src/main/kotlin/com/ttasjwi/board/Main.kt
@@ -0,0 +1,11 @@
+package com.ttasjwi.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardBeApplication>(*args)
+}
