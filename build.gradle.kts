513ab0be9cc0c644d29cc5e49c92eef58aeac3f6
ttasjwi <ttasjwi920@gmail.com>
2024-02-28 23:49:55 +0900

    build: (#4) 데이터베이스 접근기술 및 쿼리 로깅 환경 구성

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
+
+    id("org.jetbrains.kotlin.kapt") version "1.9.22"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "com.ttasjwi"
@@ -19,10 +22,28 @@ repositories {
 }
 
 dependencies {
+    // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // p6spy
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.1")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/ttasjwi/board/Main.kt
+++ src/main/kotlin/com/ttasjwi/board/Main.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BoardBeApplication
+class Main
 
 fun main(args: Array<String>) {
-    runApplication<BoardBeApplication>(*args)
+    runApplication<Main>(*args)
 }
