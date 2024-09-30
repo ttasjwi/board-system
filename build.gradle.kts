4e16c88315785ef31efe75e3c03ed1381ba83e05
ttasjwi <ttasjwi920@gmail.com>
2024-09-30 15:25:40 +0900

    Chore: (BRD-64) 멀티모듈 구조에 맞게 빌드 설정파일 변경
    Chore: (BRD-64) 프로젝트 모든 모듈에 대해서 테스트가 작동하도록 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,55 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// 루트 프로젝트
 plugins {
     id(Plugins.KOTLIN_JVM.id) version Plugins.KOTLIN_JVM.version
-    id(Plugins.KOTLIN_SPRING.id) version Plugins.KOTLIN_SPRING.version
-    id(Plugins.SPRING_BOOT.id) version Plugins.SPRING_BOOT.version
+    id(Plugins.KOTLIN_SPRING.id) version Plugins.KOTLIN_SPRING.version apply false
+    id(Plugins.SPRING_BOOT.id) version Plugins.SPRING_BOOT.version apply false
     id(Plugins.SPRING_DEPENDENCY_MANAGEMENT.id) version Plugins.SPRING_DEPENDENCY_MANAGEMENT.version
 }
-
-group = ProjectProperties.GROUP_NAME
-version = ProjectProperties.VERSION
 
 java {
     sourceCompatibility = JavaVersion.valueOf("VERSION_${ProjectProperties.JAVA_VERSION}")
 }
 
-repositories {
-    mavenCentral()
-}
+// 루트 프로젝트 + 서브 프로젝트 전체
+allprojects {
+    group = ProjectProperties.GROUP_NAME
+    version = ProjectProperties.VERSION
 
-dependencies {
-    implementation(Dependencies.SPRING_BOOT_WEB.fullName)
-    implementation(Dependencies.KOTLIN_REFLECT.fullName)
-    implementation(Dependencies.KOTLIN_JACKSON.fullName)
-    testImplementation(Dependencies.SPRING_BOOT_TEST.fullName)
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = ProjectProperties.JAVA_VERSION
+    repositories {
+        mavenCentral()
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
+// 서브프로젝트에 적용
+subprojects {
+    apply { plugin(Plugins.KOTLIN_JVM.id) }
+    apply { plugin(Plugins.KOTLIN_SPRING.id) }
+    apply { plugin(Plugins.SPRING_BOOT.id) }
+    apply { plugin(Plugins.SPRING_DEPENDENCY_MANAGEMENT.id) }
 
-tasks.getByName("bootJar") {
-    enabled = true
-}
+    dependencies {
+        implementation(Dependencies.KOTLIN_REFLECT.fullName)
+        testImplementation(Dependencies.SPRING_BOOT_TEST.fullName)
+    }
 
-tasks.getByName("jar") {
-    enabled = false
+    tasks.getByName("bootJar") {
+        enabled = false
+    }
+
+    tasks.getByName("jar") {
+        enabled = true
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = ProjectProperties.JAVA_VERSION
+        }
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "board-system"
+
+include(
+
+    // 설정 최종 구성 및 실행
+    "board-system-container",
+
+    // api
+    "board-system-api:api-deploy",
+)
