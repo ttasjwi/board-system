f35863e3edecb343962d8fca909d8233d4d7df68
ttasjwi <ttasjwi920@gmail.com>
2024-10-16 09:19:39 +0900

    Feature: (BRD-36) external-message 모듈 정의

--- board-system-container/build.gradle.kts
+++ board-system-container/build.gradle.kts
@@ -1,6 +1,11 @@
 dependencies {
     implementation(Dependencies.SPRING_BOOT_STARTER.fullName)
+
+    // api
     implementation(project(":board-system-api:api-deploy"))
+
+    // external
+    implementation(project(":board-system-external:external-message"))
 }
 
 tasks.getByName("bootJar") {
