9be0635281134520e72c73d97bae2832c04815e5
ttasjwi <ttasjwi920@gmail.com>
2024-10-26 16:43:32 +0900

    Feature: (BRD-32) external-db - MemberFinder, MemberAppender 구현체 골격 작성

--- board-system-external/external-db/build.gradle.kts
+++ board-system-external/external-db/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
+    implementation(project(":board-system-domain:domain-core"))
+    implementation(project(":board-system-domain:domain-member"))
     implementation(project(":board-system-application:application-core"))
 }
