34a7651e4c23f1f84936ab4075c8f4bc230d7949
ttasjwi <ttasjwi920@gmail.com>
2024-10-27 15:18:02 +0900

    Test: (BRD-32) domain-email - 이메일 도메인 모델 기본 테스트 작성

---
+++ board-system-domain/domain-email/src/test/kotlin/com/ttasjwi/board/system/email/domain/event/fixture/EmailVerificationStartedEventFixture.kt
@@ -0,0 +1,19 @@
+package com.ttasjwi.board.system.email.domain.event.fixture
+
+import com.ttasjwi.board.system.core.time.fixture.timeFixture
+import com.ttasjwi.board.system.email.domain.event.EmailVerificationStartedEvent
+import java.time.ZonedDateTime
+
+fun emailVerificationStartedEventFixture(
+    email: String = "hello@gmail.com",
+    code: String = "code12345",
+    codeCreatedAt: ZonedDateTime = timeFixture(minute = 0),
+    codeExpiresAt: ZonedDateTime = codeCreatedAt.plusMinutes(5),
+): EmailVerificationStartedEvent {
+    return EmailVerificationStartedEvent(
+        email = email,
+        code = code,
+        codeCreatedAt = codeCreatedAt,
+        codeExpiresAt = codeExpiresAt,
+    )
+}
---
+++ board-system-domain/domain-email/src/test/kotlin/com/ttasjwi/board/system/email/domain/model/fixture/EmailVerificationFixture.kt
@@ -0,0 +1,39 @@
+package com.ttasjwi.board.system.email.domain.model.fixture
+
+import com.ttasjwi.board.system.core.time.fixture.timeFixture
+import com.ttasjwi.board.system.email.domain.model.EmailVerification
+import java.time.ZonedDateTime
+
+fun emailVerificationFixtureNotVerified(
+    email: String = "test@gmail.com",
+    code: String = "1111",
+    codeCreatedAt: ZonedDateTime = timeFixture(minute = 0),
+    codeExpiresAt: ZonedDateTime = codeCreatedAt.plusMinutes(5),
+): EmailVerification {
+    return EmailVerification(
+        email = emailFixture(email),
+        code = code,
+        codeCreatedAt = codeCreatedAt,
+        codeExpiresAt = codeExpiresAt,
+        verifiedAt = null,
+        verificationExpiresAt = null
+    )
+}
+
+fun emailVerificationFixtureVerified(
+    email: String = "test@gmail.com",
+    code: String = "1111",
+    codeCreatedAt: ZonedDateTime = timeFixture(minute = 0),
+    codeExpiresAt: ZonedDateTime = codeCreatedAt.plusMinutes(5),
+    verifiedAt: ZonedDateTime = timeFixture(minute = 1),
+    verificationExpiresAt: ZonedDateTime = verifiedAt.plusMinutes(30)
+): EmailVerification {
+    return EmailVerification(
+        email = emailFixture(email),
+        code = code,
+        codeCreatedAt = codeCreatedAt,
+        codeExpiresAt = codeExpiresAt,
+        verifiedAt = verifiedAt,
+        verificationExpiresAt = verificationExpiresAt
+    )
+}
