12a1a21dd0395b0bf6fe60504c130f8e8b8badb4
ttasjwi <ttasjwi920@gmail.com>
2024-10-30 17:48:09 +0900

    Feature: (BRD-42) UsernameCreator 구현

--- board-system-domain/domain-core/src/main/kotlin/com/ttasjwi/board/system/member/domain/model/Username.kt
+++ board-system-domain/domain-core/src/main/kotlin/com/ttasjwi/board/system/member/domain/model/Username.kt
@@ -1,5 +1,7 @@
 package com.ttasjwi.board.system.member.domain.model
 
+import com.ttasjwi.board.system.member.domain.exception.InvalidUsernameFormatException
+
 /**
  * 사용자 관점 아이디
  */
@@ -9,7 +11,29 @@ internal constructor(
 ) {
 
     companion object {
+
+        /**
+         * 영어 소문자, 숫자, 언더바만 허용. 공백 허용 안 됨.
+         */
+
+        private val pattern = Regex("^[a-z0-9_]+\$")
+        internal const val MIN_LENGTH = 4
+        internal const val MAX_LENGTH = 15
+
+        /**
+         * Username 복원
+         */
         fun restore(value: String): Username {
+            return Username(value)
+        }
+
+        /**
+         * Username 생성
+         */
+        internal fun create(value: String): Username {
+            if (value.length < MIN_LENGTH || value.length > MAX_LENGTH || !value.matches(pattern)) {
+                throw InvalidUsernameFormatException(value)
+            }
             return Username(value)
         }
     }
