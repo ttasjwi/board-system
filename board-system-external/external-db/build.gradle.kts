70d61f8cfeae3bf0e0472f56ddfb01c8e0824db3
ttasjwi <ttasjwi920@gmail.com>
2024-11-04 19:08:37 +0900

    Feature: (BRD-68) 설정파일 작성

--- board-system-external/external-db/build.gradle.kts
+++ board-system-external/external-db/build.gradle.kts
@@ -1,4 +1,11 @@
+plugins {
+    id(Plugins.KOTLIN_JPA.id) version Plugins.KOTLIN_JPA.version
+}
+
 dependencies {
+    implementation(Dependencies.SPRING_BOOT_DATA_JPA.fullName)
+    runtimeOnly(Dependencies.MYSQL_DRIVER.fullName)
+    implementation(Dependencies.P6SPY_DATASOURCE_DECORATOR.fullName)
     implementation(project(":board-system-domain:domain-core"))
     implementation(project(":board-system-domain:domain-member"))
     implementation(project(":board-system-application:application-core"))
