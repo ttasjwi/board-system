0944fed2b63a21c23885d09e90c7f698cd2285d7
ttasjwi <ttasjwi920@gmail.com>
2024-11-05 13:39:23 +0900

    Refactor: (BRD-70) 사용자 로케일 획득 인터페이스 분리

--- board-system-core/src/testFixtures/kotlin/com/ttasjwi/board/system/core/message/fixture/MessageResolverFixture.kt
+++ board-system-core/src/testFixtures/kotlin/com/ttasjwi/board/system/core/message/fixture/MessageResolverFixture.kt
@@ -1,14 +1,15 @@
 package com.ttasjwi.board.system.core.message.fixture
 
 import com.ttasjwi.board.system.core.message.MessageResolver
+import java.util.*
 
 class MessageResolverFixture : MessageResolver {
 
-    override fun resolveMessage(code: String): String {
-        return "$code.message"
+    override fun resolveMessage(code: String, locale: Locale): String {
+        return "$code.message(locale=${locale})"
     }
 
-    override fun resolveDescription(code: String, args: List<Any?>): String {
-        return "$code.description(args=$args)"
+    override fun resolveDescription(code: String, args: List<Any?>, locale: Locale): String {
+        return "$code.description(args=$args,locale=$locale)"
     }
 }
