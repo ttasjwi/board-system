71cdddb209d676027194f82d87a30a92832410e7
ttasjwi <ttasjwi920@gmail.com>
2024-11-05 15:14:10 +0900

    Feature: (BRD-43) 이메일 인증 시작됨 이벤트 사양 수정(로케일 포함)

--- board-system-domain/domain-member/src/main/kotlin/com/ttasjwi/board/system/member/domain/event/EmailVerificationStartedEvent.kt
+++ board-system-domain/domain-member/src/main/kotlin/com/ttasjwi/board/system/member/domain/event/EmailVerificationStartedEvent.kt
@@ -2,15 +2,18 @@ package com.ttasjwi.board.system.member.domain.event
 
 import com.ttasjwi.board.system.core.domain.event.DomainEvent
 import java.time.ZonedDateTime
+import java.util.*
 
-class EmailVerificationStartedEvent(
+class EmailVerificationStartedEvent
+internal constructor(
     email: String,
     code: String,
     codeCreatedAt: ZonedDateTime,
     codeExpiresAt: ZonedDateTime,
+    locale: Locale,
 ) : DomainEvent<EmailVerificationStartedEvent.Data>(
     occurredAt = codeCreatedAt,
-    data = Data(email, code, codeCreatedAt, codeExpiresAt)
+    data = Data(email, code, codeCreatedAt, codeExpiresAt, locale)
 ) {
 
     class Data(
@@ -18,5 +21,6 @@ class EmailVerificationStartedEvent(
         val code: String,
         val codeCreatedAt: ZonedDateTime,
         val codeExpiresAt: ZonedDateTime,
+        val locale: Locale
     )
 }
