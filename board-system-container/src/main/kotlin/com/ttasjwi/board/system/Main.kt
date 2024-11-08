4b8d4f435801cd6a2bd2048d4b73f7c2f51e90ef
ttasjwi <ttasjwi920@gmail.com>
2024-11-08 09:41:12 +0900

    Feature: (BRD-43) 이메일 인증 시작됨 이벤트 비동기 구독자 기능 구현

--- board-system-container/src/main/kotlin/com/ttasjwi/board/system/Main.kt
+++ board-system-container/src/main/kotlin/com/ttasjwi/board/system/Main.kt
@@ -6,7 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.FilterType
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @ConfigurationPropertiesScan
 @ComponentScan(
     includeFilters = [ComponentScan.Filter(
--- board-system-event/event-consumer-member/src/main/kotlin/com/ttasjwi/board/system/member/event/consumer/EmailVerificationStartedEventConsumer.kt
+++ board-system-event/event-consumer-member/src/main/kotlin/com/ttasjwi/board/system/member/event/consumer/EmailVerificationStartedEventConsumer.kt
@@ -1,14 +1,25 @@
 package com.ttasjwi.board.system.member.event.consumer
 
+import com.ttasjwi.board.system.core.message.MessageResolver
+import com.ttasjwi.board.system.member.application.usecase.EmailSendUseCase
 import com.ttasjwi.board.system.member.domain.event.EmailVerificationStartedEvent
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
-class EmailVerificationStartedEventConsumer {
+class EmailVerificationStartedEventConsumer(
+    private val emailSendUseCase: EmailSendUseCase,
+    private val messageResolver: MessageResolver,
+) {
 
+    @Async
     @EventListener(EmailVerificationStartedEvent::class)
     fun handleEmailVerificationStartedEvent(event: EmailVerificationStartedEvent) {
-        TODO("not implemented")
+        emailSendUseCase.sendEmail(
+            address = event.data.email,
+            subject = messageResolver.resolveMessage("EmailVerification.subject", event.data.locale),
+            content = messageResolver.resolveDescription("EmailVerification.content", listOf(event.data.code), event.data.locale),
+        )
     }
 }
