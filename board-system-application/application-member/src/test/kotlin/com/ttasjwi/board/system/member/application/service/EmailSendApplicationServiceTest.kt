843c1523fb4139341e02ecd4cffb495c69d60257
ttasjwi <ttasjwi920@gmail.com>
2024-11-08 09:32:50 +0900

    Feature: (BRD-43) 이메일 발송 애플리케이션 기능 구현 및 테스트

---
+++ board-system-application/application-member/src/test/kotlin/com/ttasjwi/board/system/member/application/service/EmailSendApplicationServiceTest.kt
@@ -0,0 +1,30 @@
+package com.ttasjwi.board.system.member.application.service
+
+import com.ttasjwi.board.system.member.domain.service.fixture.EmailCreatorFixture
+import com.ttasjwi.board.system.member.domain.service.fixture.EmailSenderFixture
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("EmailSendApplicationService 테스트")
+class EmailSendApplicationServiceTest {
+
+    private lateinit var emailSendApplicationService: EmailSendApplicationService
+
+    @BeforeEach
+    fun setup() {
+        emailSendApplicationService = EmailSendApplicationService(
+            emailCreator = EmailCreatorFixture(),
+            emailSender = EmailSenderFixture()
+        )
+    }
+
+    @Test
+    @DisplayName("sendEmail : 도메인서비스를 통해 이메일 발송을 위임")
+    fun test() {
+        val email = "hello@gmail.com"
+        val subject = "제목"
+        val content = "본문"
+        emailSendApplicationService.sendEmail(email, subject, content)
+    }
+}
