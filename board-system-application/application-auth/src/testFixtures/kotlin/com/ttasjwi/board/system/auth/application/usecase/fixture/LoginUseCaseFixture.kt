510207f09b026bd7e7697a885d335c0eadab7e94
ttasjwi <ttasjwi920@gmail.com>
2024-11-09 23:43:44 +0900

    Test: (BRD-7) 로그인 유즈케이스 픽스쳐 작성

---
+++ board-system-application/application-auth/src/testFixtures/kotlin/com/ttasjwi/board/system/auth/application/usecase/fixture/LoginUseCaseFixture.kt
@@ -0,0 +1,18 @@
+package com.ttasjwi.board.system.auth.application.usecase.fixture
+
+import com.ttasjwi.board.system.auth.application.usecase.LoginRequest
+import com.ttasjwi.board.system.auth.application.usecase.LoginResult
+import com.ttasjwi.board.system.auth.application.usecase.LoginUseCase
+import com.ttasjwi.board.system.core.time.fixture.timeFixture
+
+class LoginUseCaseFixture : LoginUseCase {
+
+    override fun login(request: LoginRequest): LoginResult {
+        return LoginResult(
+            accessToken = "accessToken",
+            accessTokenExpiresAt = timeFixture(minute = 30),
+            refreshToken = "refreshToken",
+            refreshTokenExpiresAt = timeFixture(dayOfMonth = 2),
+        )
+    }
+}
