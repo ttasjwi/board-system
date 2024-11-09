f597950ae5d5b47c927377003e5c61dd1962f338
ttasjwi <ttasjwi920@gmail.com>
2024-11-09 12:24:49 +0900

    Test: (BRD-45) 회원가입 유즈케이스 픽스쳐 작성

---
+++ board-system-application/application-member/src/testFixtures/kotlin/com/ttasjwi/board/system/member/application/usecase/fixture/RegisterMemberUseCaseFixture.kt
@@ -0,0 +1,20 @@
+package com.ttasjwi.board.system.member.application.usecase.fixture
+
+import com.ttasjwi.board.system.core.time.fixture.timeFixture
+import com.ttasjwi.board.system.member.application.usecase.RegisterMemberRequest
+import com.ttasjwi.board.system.member.application.usecase.RegisterMemberResult
+import com.ttasjwi.board.system.member.application.usecase.RegisterMemberUseCase
+
+class RegisterMemberUseCaseFixture : RegisterMemberUseCase {
+
+    override fun register(request: RegisterMemberRequest): RegisterMemberResult {
+        return RegisterMemberResult(
+            memberId = 1L,
+            email = request.email!!,
+            username = request.username!!,
+            nickname = request.nickname!!,
+            role = "USER",
+            registeredAt = timeFixture(minute = 6)
+        )
+    }
+}
