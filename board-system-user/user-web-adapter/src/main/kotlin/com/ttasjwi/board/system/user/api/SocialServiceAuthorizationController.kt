326be86a3ed81039193b53ef94bd5ccfed4a205d
ttasjwi <ttasjwi920@gmail.com>
2025-05-03 19:05:16 +0900

    Test: (BRD-122) 소셜서비스 인가 API 문서화 테스트

--- board-system-user/user-web-adapter/src/main/kotlin/com/ttasjwi/board/system/user/api/SocialServiceAuthorizationController.kt
+++ board-system-user/user-web-adapter/src/main/kotlin/com/ttasjwi/board/system/user/api/SocialServiceAuthorizationController.kt
@@ -1,11 +1,12 @@
 package com.ttasjwi.board.system.user.api
 
+import com.ttasjwi.board.system.common.annotation.auth.PermitAll
 import com.ttasjwi.board.system.user.domain.SocialServiceAuthorizationUseCase
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,7 +14,8 @@ class SocialServiceAuthorizationController(
     private val socialServiceAuthorizationUseCase: SocialServiceAuthorizationUseCase
 ) {
 
-    @PostMapping("/api/v1/auth/social-service-authorization/{socialServiceId}")
+    @PermitAll
+    @GetMapping("/api/v1/auth/social-service-authorization/{socialServiceId}")
     fun authorize(@PathVariable("socialServiceId") socialServiceId: String): ResponseEntity<Void> {
         val url = socialServiceAuthorizationUseCase.generateAuthorizationRequestUri(socialServiceId)
         return ResponseEntity
