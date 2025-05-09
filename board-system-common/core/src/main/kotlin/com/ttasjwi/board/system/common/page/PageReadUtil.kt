b52a0e7c0efa0fe0fe0b20b8245bfa76534bc4e8
ttasjwi <ttasjwi920@gmail.com>
2025-05-09 11:23:32 +0900

    Feature: (BRD-87) 페이징 관련 계산 유틸함수 작성

---
+++ board-system-common/core/src/main/kotlin/com/ttasjwi/board/system/common/page/PageReadUtil.kt
@@ -0,0 +1,9 @@
+package com.ttasjwi.board.system.common.page
+
+fun calculatePageLimit(page: Long, pageSize: Long, movablePageCount: Long): Long =
+    (((page - 1) / movablePageCount) + 1) * pageSize * movablePageCount + 1
+
+
+fun calculateOffSet(page: Long, pageSize: Long): Long =
+    (page - 1) * pageSize
+
