5920199500023d47b2218f95b5aacf05555617d2
ttasjwi <ttasjwi920@gmail.com>
2025-05-10 15:46:36 +0900

    Test: (BRD-128) 닉네임 유효성 확인 API 명세 문서화(RestDocs) (#83)

--- board-system-user/user-web-adapter/src/test/kotlin/com/ttasjwi/board/system/user/api/NicknameAvailableControllerTest.kt
+++ board-system-user/user-web-adapter/src/test/kotlin/com/ttasjwi/board/system/user/api/NicknameAvailableControllerTest.kt
@@ -1,40 +1,84 @@
 package com.ttasjwi.board.system.user.api
 
-import com.ttasjwi.board.system.user.domain.NicknameAvailableRequest
-import com.ttasjwi.board.system.user.domain.NicknameAvailableResponse
-import com.ttasjwi.board.system.user.domain.fixture.NicknameAvailableUseCaseFixture
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
+import com.ninjasquad.springmockk.MockkBean
+import com.ttasjwi.board.system.test.util.*
+import com.ttasjwi.board.system.user.domain.*
+import com.ttasjwi.board.system.user.test.base.UserRestDocsTest
+import io.mockk.every
+import io.mockk.verify
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.http.HttpStatus
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpMethod
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.request
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@DisplayName("NicknameAvailableController 테스트")
-class NicknameAvailableControllerTest {
+@DisplayName("[user-web-adapter] NicknameAvailableController 테스트")
+@WebMvcTest(NicknameAvailableController::class)
+class NicknameAvailableControllerTest : UserRestDocsTest() {
 
-    private lateinit var controller: NicknameAvailableController
-
-    @BeforeEach
-    fun setup() {
-        controller = NicknameAvailableController(NicknameAvailableUseCaseFixture())
-    }
+    @MockkBean
+    private lateinit var nicknameAvailableUseCase: NicknameAvailableUseCase
 
     @Test
-    @DisplayName("유즈케이스를 호출하고 200 응답을 반환한다.")
-    fun test() {
+    @DisplayName("성공 테스트")
+    fun testSuccess() {
         // given
-        val request = NicknameAvailableRequest(nickname = "hello")
+        val url = "/api/v1/users/nickname-available"
+
+        val request = NicknameAvailableRequest(
+            nickname = "땃쥐"
+        )
+
+        val response = NicknameAvailableResponse(
+            yourNickname = request.nickname!!,
+            isAvailable = true,
+            reasonCode = "NicknameAvailableCheck.Available",
+            reasonMessage = "사용 가능한 닉네임",
+            reasonDescription = "이 닉네임은 사용 가능합니다."
+        )
+
+        every { nicknameAvailableUseCase.checkNicknameAvailable(request) } returns response
 
         // when
-        val responseEntity = controller.checkNicknameAvailable(request)
-        val response = responseEntity.body as NicknameAvailableResponse
-
         // then
-        assertThat(responseEntity.statusCode.value()).isEqualTo(HttpStatus.OK.value())
-        assertThat(response.yourNickname).isEqualTo(request.nickname)
-        assertThat(response.isAvailable).isEqualTo(true)
-        assertThat(response.reasonCode).isEqualTo("NicknameAvailableCheck.Available")
-        assertThat(response.reasonMessage).isEqualTo("사용 가능한 닉네임")
-        assertThat(response.reasonDescription).isEqualTo("이 닉네임은 사용 가능합니다.")
+        mockMvc.perform(
+            request(HttpMethod.GET, url)
+                .queryParam("nickname", request.nickname)
+        )
+            .andExpectAll(
+                status().isOk,
+                jsonPath("$.yourNickname").value(response.yourNickname),
+                jsonPath("$.isAvailable").value(response.isAvailable),
+                jsonPath("$.reasonCode").value(response.reasonCode),
+                jsonPath("$.reasonMessage").value(response.reasonMessage),
+                jsonPath("$.reasonDescription").value(response.reasonDescription),
+            )
+            .andDocument(
+                identifier = "nickname-available-success",
+                queryParameters(
+                    "nickname"
+                            paramMeans "유효성 확인 대상이 되는 닉네임"
+                ),
+                responseBody(
+                    "yourNickname"
+                            type STRING
+                            means "요청 시 전달한 닉네임",
+                    "isAvailable"
+                            type BOOLEAN
+                            means "닉네임 사용이 가능한지 여부(부울값)",
+                    "reasonCode"
+                            type STRING
+                            means "닉네임 사용 가능/불가능 시 그 이유를 설명하는 코드",
+                    "reasonMessage"
+                            type STRING
+                            means "reasonCode 를 설명하는 간단한 사유 메시지",
+                    "reasonDescription"
+                            type STRING
+                            means "reasonCode 를 설명하는 상세한 사유 메시지"
+                )
+            )
+        verify(exactly = 1) { nicknameAvailableUseCase.checkNicknameAvailable(request) }
     }
 }
