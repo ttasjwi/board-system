4d1f43c7a98ad8f1e74448b7e04d4a44ab988289
ttasjwi <ttasjwi920@gmail.com>
2025-05-10 17:05:52 +0900

    Test: (BRD-131) 토큰 재갱신 API 명세 문서화(RestDocs)

--- board-system-user/user-web-adapter/src/test/kotlin/com/ttasjwi/board/system/user/api/TokenRefreshControllerTest.kt
+++ board-system-user/user-web-adapter/src/test/kotlin/com/ttasjwi/board/system/user/api/TokenRefreshControllerTest.kt
@@ -1,41 +1,92 @@
 package com.ttasjwi.board.system.user.api
 
+import com.ninjasquad.springmockk.MockkBean
+import com.ttasjwi.board.system.common.time.fixture.appDateTimeFixture
+import com.ttasjwi.board.system.test.util.*
 import com.ttasjwi.board.system.user.domain.TokenRefreshRequest
 import com.ttasjwi.board.system.user.domain.TokenRefreshResponse
-import com.ttasjwi.board.system.user.domain.fixture.TokenRefreshUseCaseFixture
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
+import com.ttasjwi.board.system.user.domain.TokenRefreshUseCase
+import com.ttasjwi.board.system.user.test.base.UserRestDocsTest
+import io.mockk.every
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.http.HttpStatus
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.request
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@DisplayName("TokenRefreshController 테스트")
-class TokenRefreshControllerTest {
+@DisplayName("[user-web-adapter] TokenRefreshController 테스트")
+@WebMvcTest(TokenRefreshController::class)
+class TokenRefreshControllerTest : UserRestDocsTest() {
 
-    private lateinit var controller: TokenRefreshController
+    @MockkBean
+    private lateinit var tokenRefreshUseCase: TokenRefreshUseCase
 
-    @BeforeEach
-    fun setup() {
-        controller = TokenRefreshController(TokenRefreshUseCaseFixture())
-    }
 
     @Test
-    @DisplayName("유즈케이스를 호출하고 200 응답을 반환한다.")
-    fun test() {
+    @DisplayName("성공 테스트")
+    fun testSuccess() {
         // given
-        val request = TokenRefreshRequest("token")
+        val url = "/api/v1/auth/token-refresh"
+
+        val request = TokenRefreshRequest("refreshToken")
+        val response = TokenRefreshResponse(
+            accessToken = "newAccessToken",
+            accessTokenType = "Bearer",
+            accessTokenExpiresAt = appDateTimeFixture(minute = 37).toZonedDateTime(),
+            refreshToken = request.refreshToken!!,
+            refreshTokenExpiresAt = appDateTimeFixture(dayOfMonth = 2, minute = 32).toZonedDateTime(),
+            refreshTokenRefreshed = false,
+        )
+
+        every { tokenRefreshUseCase.tokenRefresh(request) } returns response
 
         // when
-        val responseEntity = controller.tokenRefresh(request)
-        val response = responseEntity.body as TokenRefreshResponse
-
         // then
-        assertThat(responseEntity.statusCode.value()).isEqualTo(HttpStatus.OK.value())
-        assertThat(response.accessToken).isNotNull()
-        assertThat(response.accessTokenType).isNotNull()
-        assertThat(response.accessTokenExpiresAt).isNotNull()
-        assertThat(response.refreshToken).isNotNull()
-        assertThat(response.refreshTokenExpiresAt).isNotNull()
-        assertThat(response.refreshTokenExpiresAt).isNotNull()
+        mockMvc
+            .perform(
+                request(HttpMethod.POST, url)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(serializeToJson(request))
+            )
+            .andExpectAll(
+                status().isOk,
+                jsonPath("$.accessToken").value(response.accessToken),
+                jsonPath("$.accessTokenExpiresAt").value("2025-01-01T00:37:00+09:00"),
+                jsonPath("$.accessTokenType").value(response.accessTokenType),
+                jsonPath("$.refreshToken").value(response.refreshToken),
+                jsonPath("$.refreshTokenExpiresAt").value("2025-01-02T00:32:00+09:00"),
+                jsonPath("$.refreshTokenRefreshed").value(response.refreshTokenRefreshed),
+            )
+            .andDocument(
+                identifier = "token-refresh-success",
+                requestBody(
+                    "refreshToken"
+                            type STRING
+                            means "현재 유효한 리프레시토큰 값"
+                ),
+                responseBody(
+                    "accessToken"
+                            type STRING
+                            means "액세스 토큰",
+                    "accessTokenExpiresAt"
+                            type DATETIME
+                            means "액세스 토큰 만료시각",
+                    "accessTokenType"
+                            type STRING
+                            means "액세스 토큰 타입",
+                    "refreshToken"
+                            type STRING
+                            means "리프레시 토큰",
+                    "refreshTokenExpiresAt"
+                            type DATETIME
+                            means "리프레시 토큰 만료시각",
+                    "refreshTokenRefreshed"
+                            type BOOLEAN
+                            means "리프레시 토큰이 재갱신됐는 지 여부"
+                )
+            )
     }
 }
