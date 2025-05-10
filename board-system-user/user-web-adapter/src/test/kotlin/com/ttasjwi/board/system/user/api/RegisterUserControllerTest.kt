01e1269134ac68ce63d698852d6ac219c5b675eb
ttasjwi <ttasjwi920@gmail.com>
2025-05-10 16:57:14 +0900

    Test: (BRD-130) 회원가입 API 명세 문서화(RestDocs) (#86)

--- board-system-user/user-web-adapter/src/test/kotlin/com/ttasjwi/board/system/user/api/RegisterUserControllerTest.kt
+++ board-system-user/user-web-adapter/src/test/kotlin/com/ttasjwi/board/system/user/api/RegisterUserControllerTest.kt
@@ -1,46 +1,111 @@
 package com.ttasjwi.board.system.user.api
 
+import com.ninjasquad.springmockk.MockkBean
+import com.ttasjwi.board.system.common.time.fixture.appDateTimeFixture
+import com.ttasjwi.board.system.test.util.*
 import com.ttasjwi.board.system.user.domain.RegisterUserRequest
 import com.ttasjwi.board.system.user.domain.RegisterUserResponse
-import com.ttasjwi.board.system.user.domain.fixture.RegisterUserUseCaseFixture
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
+import com.ttasjwi.board.system.user.domain.RegisterUserUseCase
+import com.ttasjwi.board.system.user.test.base.UserRestDocsTest
+import io.mockk.every
+import io.mockk.verify
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.http.HttpStatus
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.request
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@DisplayName("RegisterUserController 테스트")
-class RegisterUserControllerTest {
+@DisplayName("[user-web-adapter] RegisterUserController 테스트")
+@WebMvcTest(RegisterUserController::class)
+class RegisterUserControllerTest : UserRestDocsTest() {
 
-    private lateinit var controller: RegisterUserController
-
-    @BeforeEach
-    fun setup() {
-        controller = RegisterUserController(RegisterUserUseCaseFixture())
-    }
+    @MockkBean
+    private lateinit var registerUserUseCase: RegisterUserUseCase
 
     @Test
-    @DisplayName("유즈케이스를 호출하고 201 응답을 반환한다.")
-    fun test() {
+    @DisplayName("성공 테스트")
+    fun testSuccess() {
         // given
+        val url = "/api/v1/users"
         val request = RegisterUserRequest(
-            email = "test@test.com",
-            password = "1111",
-            username = "testuser",
-            nickname = "testnick"
+            email = "hello@gmail.com",
+            password = "1234",
+            username = "ttasjwi",
+            nickname = "땃쥐"
         )
 
-        // when
-        val responseEntity = controller.register(request)
-        val response = responseEntity.body as RegisterUserResponse
+        val response = RegisterUserResponse(
+            userId = "1",
+            email = request.email!!,
+            username = request.username!!,
+            nickname = request.nickname!!,
+            role = "USER",
+            registeredAt = appDateTimeFixture(minute = 29).toZonedDateTime()
+        )
 
+        every { registerUserUseCase.register(request) } returns response
+
+        // when
         // then
-        assertThat(responseEntity.statusCode.value()).isEqualTo(HttpStatus.CREATED.value())
-        assertThat(response.userId).isNotNull()
-        assertThat(response.email).isEqualTo(request.email)
-        assertThat(response.username).isEqualTo(request.username)
-        assertThat(response.nickname).isEqualTo(request.nickname)
-        assertThat(response.role).isNotNull()
-        assertThat(response.registeredAt).isNotNull()
+        mockMvc
+            .perform(
+                request(HttpMethod.POST, url)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(serializeToJson(request))
+            )
+            .andExpectAll(
+                status().isCreated,
+                jsonPath("$.userId").value(response.userId),
+                jsonPath("$.email").value(response.email),
+                jsonPath("$.username").value(response.username),
+                jsonPath("$.nickname").value(response.nickname),
+                jsonPath("$.role").value(response.role),
+                jsonPath("$.registeredAt").value("2025-01-01T00:29:00+09:00"),
+            )
+            .andDocument(
+                identifier = "register-user-success",
+                requestBody(
+                    "email"
+                            type STRING
+                            means "이메일"
+                            constraint "유효한 포맷이어야 하고, 인증된(현재 유효한) 이메일이어야 함. ",
+                    "password"
+                            type STRING
+                            means "패스워드"
+                            constraint "4자 이상 32자 이하의 문자열",
+                    "username"
+                            type STRING
+                            means "사용자아이디(username)"
+                            constraint "4자 이상 15자 이하, 영어 소문자, 숫자, 언더바만 허용. 공백 허용 안 됨.",
+                    "nickname"
+                            type STRING
+                            means "닉네임"
+                            constraint "1자 이상 15자 이하, 한글, 영문자(대,소), 숫자만 허용"
+                ),
+                responseBody(
+                    "userId"
+                            type STRING
+                            means "사용자 식별자(Id)",
+                    "email"
+                            type STRING
+                            means "사용자 이메일",
+                    "username"
+                            type STRING
+                            means "사용자관점 아이디",
+                    "nickname"
+                            type STRING
+                            means "사용자 닉네임",
+                    "role"
+                            type STRING
+                            means "사용자 역할",
+                    "registeredAt"
+                            type DATETIME
+                            means "회원 가입 시점"
+                )
+            )
+        verify(exactly = 1) { registerUserUseCase.register(request) }
     }
 }
