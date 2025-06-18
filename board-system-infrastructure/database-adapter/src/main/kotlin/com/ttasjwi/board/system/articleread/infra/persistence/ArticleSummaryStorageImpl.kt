8855e1fa25099721665dff2f631bf4825b3c12b3
ttasjwi <ttasjwi920@gmail.com>
2025-06-18 15:47:34 +0900

    Refactor: (BRD-168) 게시글 목록 무한스크롤 조회 - 쿼리 변경(커버링 인덱스 활용) (#120)

--- board-system-infrastructure/database-adapter/src/main/kotlin/com/ttasjwi/board/system/articleread/infra/persistence/ArticleSummaryStorageImpl.kt
+++ board-system-infrastructure/database-adapter/src/main/kotlin/com/ttasjwi/board/system/articleread/infra/persistence/ArticleSummaryStorageImpl.kt
@@ -1,24 +1,13 @@
 package com.ttasjwi.board.system.articleread.infra.persistence
 
-import com.querydsl.core.types.dsl.BooleanExpression
-import com.querydsl.jpa.impl.JPAQuery
-import com.querydsl.jpa.impl.JPAQueryFactory
 import com.ttasjwi.board.system.articleread.domain.model.ArticleSummaryQueryModel
 import com.ttasjwi.board.system.articleread.domain.port.ArticleSummaryStorage
-import com.ttasjwi.board.system.articleread.infra.persistence.dto.*
 import com.ttasjwi.board.system.articleread.infra.persistence.jpa.JpaArticleRepository
 import org.springframework.stereotype.Component
-import com.ttasjwi.board.system.articleread.infra.persistence.jpa.QJpaArticle.jpaArticle as article
-import com.ttasjwi.board.system.articleread.infra.persistence.jpa.QJpaArticleCategory.jpaArticleCategory as articleCategory
-import com.ttasjwi.board.system.articleread.infra.persistence.jpa.QJpaArticleCommentCount.jpaArticleCommentCount as articleCommentCount
-import com.ttasjwi.board.system.articleread.infra.persistence.jpa.QJpaArticleDislikeCount.jpaArticleDislikeCount as articleDislikeCount
-import com.ttasjwi.board.system.articleread.infra.persistence.jpa.QJpaArticleLikeCount.jpaArticleLikeCount as articleLikeCount
-import com.ttasjwi.board.system.articleread.infra.persistence.jpa.QJpaBoard.jpaBoard as board
 
 @Component
 class ArticleSummaryStorageImpl(
     private val jpaArticleRepository: JpaArticleRepository,
-    private val queryFactory: JPAQueryFactory
 ) : ArticleSummaryStorage {
 
     override fun findAllPage(boardId: Long, limit: Long, offset: Long): List<ArticleSummaryQueryModel> {
@@ -34,44 +23,9 @@ class ArticleSummaryStorageImpl(
         limit: Long,
         lastArticleId: Long?
     ): List<ArticleSummaryQueryModel> {
-        return selectArticleSummaryWithJoins()
-            .where(article.boardId.eq(boardId).and(lastArticleIdCondition(lastArticleId)))
-            .orderBy(article.articleId.desc())
-            .limit(limit)
-            .fetch()
-    }
-
-    private fun selectArticleSummaryWithJoins(): JPAQuery<QueryDslArticleSummaryQueryModel> {
-        return queryFactory
-            .select(projectArticleSummary())
-            .from(article)
-            .innerJoin(board).on(board.boardId.eq(article.boardId))
-            .innerJoin(articleCategory).on(articleCategory.articleCategoryId.eq(article.articleCategoryId))
-            .leftJoin(articleCommentCount).on(articleCommentCount.articleId.eq(article.articleId))
-            .leftJoin(articleLikeCount).on(articleLikeCount.articleId.eq(article.articleId))
-            .leftJoin(articleDislikeCount).on(articleDislikeCount.articleId.eq(article.articleId))
-    }
-
-    private fun projectArticleSummary(): QQueryDslArticleSummaryQueryModel {
-        return QQueryDslArticleSummaryQueryModel(
-            article.articleId,
-            article.title,
-            article.boardId,
-            board.name,
-            board.slug,
-            article.articleCategoryId,
-            articleCategory.name,
-            articleCategory.slug,
-            article.writerId,
-            article.writerNickname,
-            articleCommentCount.commentCount.coalesce(0L),
-            articleLikeCount.likeCount.coalesce(0L),
-            articleDislikeCount.dislikeCount.coalesce(0L),
-            article.createdAt
-        )
-    }
-
-    private fun lastArticleIdCondition(lastArticleId: Long?): BooleanExpression? {
-        return lastArticleId?.let { article.articleId.lt(it) }
+        return when (lastArticleId) {
+            null -> jpaArticleRepository.findAllInfiniteScroll(boardId, limit)
+            else -> jpaArticleRepository.findAllInfiniteScroll(boardId, lastArticleId, limit)
+        }
     }
 }
